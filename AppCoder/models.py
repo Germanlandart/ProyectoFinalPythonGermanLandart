862219a511b8dde6d0c7af64a1ca9c7737d9c0a1
gerchoL <fiumel97@hotmail.com>
2023-10-05 21:26:50 -0300

    Prueba2

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 class Terror(models.Model):
     
-    def __str__(self):
-        
-        return f"Libro: {self.libro} ------ Autor: {self.autor} ------ Year: {self.year}"
     
     libro = models.CharField(max_length=60)
     autor = models.CharField(max_length=60)
     year = models.IntegerField()
+
+    def __str__(self):
+        
+        return f"Libro: {self.libro} ------ Autor: {self.autor} ------ Year: {self.year}"
+        
 
 class CienciaFiccion(models.Model):
 
