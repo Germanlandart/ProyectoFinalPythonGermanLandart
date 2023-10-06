18224d5409726db47282b39a02680d1649cbae89
gerchoL <fiumel97@hotmail.com>
2023-10-05 21:44:50 -0300

    Working1

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -2,20 +2,20 @@ from django import forms
 
 class TerrorFormulario(forms.Form):
 
-    libros = forms.CharField()
+    libro = forms.CharField()
     autor = forms.CharField()
     year = forms.IntegerField()
 
 
 class CienciaficcionFormulario(forms.Form):
 
-    libros = forms.CharField(max_length=60)
+    libro = forms.CharField(max_length=60)
     autor = forms.CharField(max_length=60)
     year = forms.IntegerField()
 
 class FantasiaFormulario(forms.Form):
 
-    libros = forms.CharField(max_length=60)
+    libro = forms.CharField(max_length=60)
     autor = forms.CharField(max_length=60)
     year = forms.IntegerField()
  
