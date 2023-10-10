1473615ff047f60ea87631098da4262346a93e48
gerchoL <fiumel97@hotmail.com>
2023-10-09 21:19:10 -0300

    Working2

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,18 +1,24 @@
 from django.urls import path
 from AppCoder.views import *
 
+
+
 urlpatterns = [
     path("",inicio, name="Inicio"),
     path("terror/", terror, name="terror"),
     path("cienciaficcion/", cienciaficcion, name="CienciaFiccion"),
     path("fantasia/", fantasia, name="Fantasia"),
-    path("terrorFormulario/", terrorFormulario, name="Formularioterror"),
+    path("crearTerror/", crearTerror, name="TerrorCrear"),
     path("buscarLibro/", busquedaterror, name="Buscarlibro"),
     path("resultados/", resultados, name="ResultadosBusqueda"),
-    path("cienciaficcionFormulario/", cienciaficcionFormulario, name="FormularioCienciaFiccion"),
-    path("fantasiaFormulario/", fantasiaFormulario, name="Formulariofantasia"),
+    path("crearFantasia/", crearFantasia, name="FantasiaCrear"),
+    path("leerCienciaFiccion/", leerCienciaFiccion, name="CienciaFiccionLee"),
+    path("eliminarCienciaFiccion/<nombreLibro>/", eliminarCienciaFiccion, name="CienciaFiccionEliminar"),
+    path("editarCienciaFiccion/<nombreLibro>/", editarCienciaFiccion, name="CienciaFiccionEditar"),
+    path("cienciaficcion/<int:pk>", DetalleCienciaFiccion.as_view(), name="CienciaFiccionDetalle"),
+    path("cienciaficcion/list/", ListaCienciaFiccion.as_view(), name="CienciaFiccionLeer"),
+    path("cienciaficcion/crear/", CrearCienciaFiccion.as_view(), name="CienciaFiccionCrear"),
+    path("cienciaficcion/editar/<int:pk>", ActualizarCienciaFiccion.as_view(), name="CienciaFiccionActualizar"),
+    path("cienciaficcion/borrar/<int:pk>", BorrarCienciaFiccion.as_view(), name="CienciaFiccionBorrar")
+    ]
 
-    #CRUD de cienciaficcion
-    path("leerCienciaFiccion/", leerCienciaFiccion, name="CienciaFiccionLeer"),
-    
-]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from AppCoder.models import *
 from .forms import TerrorFormulario, CienciaficcionFormulario, FantasiaFormulario
-
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def inicio(request):
     return render(request, "AppCoder/inicio.html")
@@ -20,7 +22,7 @@ def fantasia(request):
     
     return render(request, "AppCoder/fantasia.html")
 
-def terrorFormulario(request):
+def crearTerror(request):
 
     if request.method == "POST":
 
@@ -41,7 +43,7 @@ def terrorFormulario(request):
         formulario1 = TerrorFormulario()    
 
 
-        return render(request, "AppCoder/terrorFormulario.html", {"form1":formulario1})
+        return render(request, "AppCoder/crearTerror.html", {"form1":formulario1})
 
 
 
@@ -67,7 +69,7 @@ def resultados(request):
 
 
 
-def cienciaficcionFormulario(request):
+def crearCienciaFiccion(request):
 
     if request.method == "POST":
 
@@ -88,10 +90,10 @@ def cienciaficcionFormulario(request):
         formulario1 = CienciaficcionFormulario()    
 
 
-    return render(request, "AppCoder/cienciaficcionFormulario.html", {"form1":formulario1})
+    return render(request, "AppCoder/crearCienciaFiccion.html", {"form1":formulario1})
 
 
-def fantasiaFormulario(request):
+def crearFantasia(request):
 
     if request.method == "POST":
 
@@ -112,7 +114,7 @@ def fantasiaFormulario(request):
         formulario1 = FantasiaFormulario()    
 
 
-    return render(request, "AppCoder/fantasiaFormulario.html", {"form1":formulario1})
+    return render(request, "AppCoder/crearFantasia.html", {"form1":formulario1})
 
 
 def leerCienciaFiccion(request):
@@ -124,9 +126,74 @@ def leerCienciaFiccion(request):
     return render(request, "AppCoder/leerCienciaFiccion.html", contexto)
 
 
-def eliminarCienciaFiccion(request):
+def eliminarCienciaFiccion(request, nombreLibro):
 
-    cienciaficcion = CienciaFiccion.objects.get
-    pass
+    cienciaficcion = CienciaFiccion.objects.get(libro=nombreLibro)
+    cienciaficcion.delete()
+
+    libros = CienciaFiccion.objects.all()
+
+    contexto = {"cienciaf":libros}
+
+    return render(request, "AppCoder/leerCienciaFiccion.html", contexto)
+
+
+
+def editarCienciaFiccion (request, nombreLibro):
+
+    cienciaficcion = CienciaFiccion.objects.get(libro=nombreLibro)
+    
+    if  request.method == "POST":
+
+        formulario1 = CienciaficcionFormulario(request.POST)
+
+        if formulario1.is_valid():
+
+            info = formulario1.cleaned_data
+
+            cienciaficcion.libro = info["libro"]
+            cienciaficcion.autor = info["autor"]
+            cienciaficcion.year = info["year"]
+
+            cienciaficcion.save()
+
+            return render(request, "AppCoder/cienciaficcion.html")
+        
+    else: 
+
+        formulario1 = CienciaficcionFormulario(initial={"nombre":cienciaficcion.libro, "autor":cienciaficcion.autor, 
+                                                        "year":cienciaficcion.year})  
+          
+
+
+    return render(request, "AppCoder/editarCienciaFiccion.html", {"form1":formulario1, "libro":nombreLibro})
+
+
+class ListaCienciaFiccion(ListView):
+
+    model = CienciaFiccion
+
+class DetalleCienciaFiccion(DetailView):
+
+    model = CienciaFiccion
+
+class CrearCienciaFiccion(CreateView):
+
+    model = CienciaFiccion
+    success_url = "/AppCoder/cienciaficcion/list"   
+    fields = ["libro", "autor", "year"]
+
+class ActualizarCienciaFiccion(UpdateView):
+
+    model = CienciaFiccion
+    success_url = "/AppCoder/cienciaficcion/list"   
+    fields = ["libro", "autor", "year"]
+
+class BorrarCienciaFiccion(DeleteView):
+
+    model = CienciaFiccion
+    success_url = "/AppCoder/cienciaficcion/list"   
+    fields = ["libro", "autor", "year"]
+           
 
 
